61f605a8d6dbf014f1654968924aa3ab0e92c963
Shazeb Suhail <shazebs7@outlook.com>
2023-01-23 03:46:34 -0700

    add shazebalopolis.console app to master repo

---
+++ shazebalopolis.console/GCU/BinarySearchTree.cs
@@ -0,0 +1,43 @@
+﻿namespace shazebalopolis.console
+{
+    // List of nodes object
+    public class BinaryTree<T>
+    {
+        // variables 
+        BinaryTreeNode<T> _root;
+
+        // constructors 
+        public BinaryTree() 
+        {
+            _root = new BinaryTreeNode<T>(); 
+        }
+
+        // methods 
+        public void add(BinaryTreeNode<T> node)
+        {
+            Console.WriteLine($"Node value: \"{node.data}\"");
+        }
+
+        public void search(string searchKey)
+        {
+            Console.WriteLine($"You requested a search on: \"{searchKey}\"");
+        }
+    }
+
+    // Node object 
+    public class BinaryTreeNode<T>
+    {
+        // properties
+        public T data { get; set; }
+
+        // constructors
+        public BinaryTreeNode() { }
+
+        public BinaryTreeNode(T _data)
+        {
+            this.data = _data;
+        }
+
+        // methods
+    }
+}
